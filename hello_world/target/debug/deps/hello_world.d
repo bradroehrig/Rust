0dc56f561bd8b7306af2c29175677a250901391a
Brad Roehrig <63978952+bradroehrig@users.noreply.github.com>
2023-07-25 07:41:31 -0500

    commit

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\632915\Documents\GitHub\rust_first\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\632915\Documents\GitHub\rust_first\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
